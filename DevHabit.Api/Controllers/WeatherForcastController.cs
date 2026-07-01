39cc0369ea81e49ae474da0f62b66f6b271db241
Eldreney <Ahmed.Yehia@raqmia.net>
2026-07-02 00:59:42 +0300

    WeatherForcast Controller

---
+++ DevHabit.Api/Controllers/WeatherForcastController.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevHabit.Api.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForcastController : ControllerBase
+{
+
+
+
+    [HttpGet]
+    public IActionResult Index()
+        => Ok("OK");
+    
+
+
+
+
+}

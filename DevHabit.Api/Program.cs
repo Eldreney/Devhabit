e62a6087a165fa6cf9c3f660667a8b919c85439d
Eldreney <Ahmed.Yehia@raqmia.net>
2026-07-01 21:22:28 +0300

    added OTel config in Program.cs

--- DevHabit.Api/Program.cs
+++ DevHabit.Api/Program.cs
@@ -1,9 +1,28 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(tracing => tracing.AddHttpClientInstrumentation().AddAspNetCoreInstrumentation())
+    .WithMetrics(metrics => metrics.AddHttpClientInstrumentation().AddAspNetCoreInstrumentation())
+    .UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeScopes = true;   
+    options.IncludeFormattedMessage = true;
+    
+});
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
